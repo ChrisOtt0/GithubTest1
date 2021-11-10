beb955f5c98e257177e3f4139bbd67a2e90cd6e3
ChrisOtt0 <sophusott@gmail.com>
2021-11-10 10:29:01 +0100

    Made more files for group access

---
+++ GithubTest1/SCO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest1
+{
+    class SCO
+    {
+        public string GetName()
+        {
+            string returnString = "Christoffer";
+            return returnString;
+        }
+    }
+}

58cc5e84e6eb4b5204ced4b95bac25728b5edc09
ChrisOtt0 <sophusott@gmail.com>
2021-11-10 09:37:41 +0100

    Made minor change to inspect what happens.

--- GithubTest1/Program.cs
+++ GithubTest1/Program.cs
@@ -6,7 +6,7 @@ namespace GithubTest1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a Github test.");
         }
     }
 }
